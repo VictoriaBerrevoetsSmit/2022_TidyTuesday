f5bda134aa2c5d7633180f05078513585452c8fa
vicjuh1212 <victoriasmit@hotmail.nl>
2022-01-25 19:43:06 +0100

    V2

---
+++ 2022_1/W1_CBSData.R
@@ -0,0 +1,24 @@
+## 
+library(tidyverse)
+library(sf)
+
+#Someone like me
+W1_Someonelikeme <- read.csv("~/Library/Mobile Documents/com~apple~CloudDocs/2022_TidyTuesday/W1_Someonelikeme.csv", sep=";")
+
+#Municipalities CBS
+municipalities <- st_read("https://geodata.nationaalgeoregister.nl/cbsgebiedsindelingen/wfs?request=GetFeature&service=WFS&version=2.0.0&typeName=cbs_gemeente_2017_gegeneraliseerd&outputFormat=json")
+
+Celebrating_graduation <- municipalities %>%  
+  left_join(W1_Someonelikeme, by = c(statcode = "RegioS"))  %>% 
+  filter(Gediplomeerden_1 > 1) %>% 
+  ggplot() + 
+  geom_sf(aes(fill = Gediplomeerden_1), size = .05) + 
+  theme_void() + 
+  theme(legend.title = element_blank()) + 
+  ggtitle(
+        label = "Celebrating graduation", 
+        subtitle = "Dutch women ages 24-25 who graduated with masters' degree in 2019-2020")
+
+
+ggsave("Celebrate2021.png", Celebrating_graduation)
+
